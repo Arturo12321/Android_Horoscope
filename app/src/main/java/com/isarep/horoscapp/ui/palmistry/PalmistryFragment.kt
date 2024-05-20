90ad6c4df385f7dcc6c772d34f55bada1992b48f
Arturo12321 <valerio.manrique@tecsup.edu.pe>
2024-05-20 18:50:35 -0500

    initial commit

---
+++ app/src/main/java/com/isarep/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,24 @@
+package com.isarep.horoscapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.isarep.horoscapp.R
+import com.isarep.horoscapp.databinding.FragmentPalmistryBinding
+
+class PalmistryFragment : Fragment() {
+
+    private  var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =  FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
